1be97264b4442392a9022b85e30c3c359e6c2996
Msksgm <msksgm3124@gmail.com>
2024-11-25 19:59:32 +0900

    chore: 新規プロジェクトを作成

---
+++ src/main/java/com/example/hands_on_java_springboot_jdbc/HandsOnJavaSpringbootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.hands_on_java_springboot_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnJavaSpringbootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnJavaSpringbootJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hands_on_java_springboot_jdbc/HandsOnJavaSpringbootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hands_on_java_springboot_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnJavaSpringbootJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
